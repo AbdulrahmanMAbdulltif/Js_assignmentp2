1b2d3129152f94ce271c6d67390949bd64f8f95d
AbdulrahmanAbdulltif <90787584+AbdulrahmanAbdulltif@users.noreply.github.com>
2023-10-04 16:38:29 +0300

    update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let calculatorObj = {
     deleteBtn: document.querySelector('.btn__delete'),
     perform: '',
 }
+let decimalPointAdded = false;
 
 function add(num1, num2) {
     return Number(num1) + Number(num2);
@@ -25,16 +26,38 @@ function multiply(num1, num2) {
     return Number(num1) * Number(num2);
 }
 
-
+document.addEventListener('DOMContentLoaded', function () {
+    calculatorObj.operators.forEach((op) => {
+        op.classList.remove('selected');
+    });
+});
 
 
 calculatorObj.numbers.forEach((num) => {
     num.addEventListener('click', () => {
         let calculatorScreen = document.querySelector('.calculator__screen')
         if (!calculatorObj.perform) {
+
+            if (num.textContent === "." && decimalPointAdded) {
+                return;
+            }
+            if (num.textContent === ".") {
+                decimalPointAdded = true;
+            }
             calculatorObj.num1 += num.textContent
             calculatorScreen.textContent = calculatorObj.num1
         } else {
+            calculatorObj.operators.forEach((op) => {
+                op.classList.remove('selected');
+            });
+
+            if (num.textContent === "." && decimalPointAdded) {
+                // Do nothing if a decimal point is already added
+                return;
+            }
+            if (num.textContent === ".") {
+                decimalPointAdded = true; // Set the flag when a decimal point is added
+            }
             calculatorObj.num2 += num.textContent
             calculatorScreen.textContent = calculatorObj.num2
         }
@@ -43,66 +66,92 @@ calculatorObj.numbers.forEach((num) => {
 
 calculatorObj.operators.forEach((operator) => {
     operator.addEventListener('click', () => {
+
+        calculatorObj.operators.forEach((op) => {
+            op.classList.remove('selected');
+        });
+
         let calculatorScreen = document.querySelector('.calculator__screen');
         if (calculatorObj.perform && calculatorObj.num1 && calculatorObj.num2) {
+            let result = 0;
+
             if (calculatorObj.perform === 'Addition') {
-                calculatorObj.num1 = '' + add(calculatorObj.num1, calculatorObj.num2);
-                calculatorObj.num2 = '';
-                calculatorScreen.textContent = calculatorObj.num1;
+                result = add(calculatorObj.num1, calculatorObj.num2);
+            } else if (calculatorObj.perform === 'Subtraction') {
+                result = subtract(calculatorObj.num1, calculatorObj.num2);
+            } else if (calculatorObj.perform === 'Multiplication') {
+                result = multiply(calculatorObj.num1, calculatorObj.num2);
+            } else if (calculatorObj.perform === 'Division') {
+                result = divide(calculatorObj.num1, calculatorObj.num2);
             }
 
-            if (calculatorObj.perform === 'Subtraction') {
-                calculatorObj.num1 = '' + subtract(calculatorObj.num1, calculatorObj.num2);
-                calculatorObj.num2 = '';
-                calculatorScreen.textContent = calculatorObj.num1;
+            if (Number.isInteger(result)) {
+                calculatorObj.num1 = result.toString(); // Convert to string to remove decimal
+            } else {
+                calculatorObj.num1 = result.toFixed(2); // Keep two decimal places
             }
-
-            if (calculatorObj.perform === 'Multiplication') {
-                calculatorObj.num1 = '' + multiply(calculatorObj.num1, calculatorObj.num2);
-                calculatorObj.num2 = '';
-                calculatorScreen.textContent = calculatorObj.num1;
-            }
-
-            if (calculatorObj.perform === 'Division') {
-                calculatorObj.num1 = '' + divide(calculatorObj.num1, calculatorObj.num2);
-                calculatorObj.num2 = '';
-                calculatorScreen.textContent = calculatorObj.num1;
-            }
+            calculatorObj.num2 = '';
+            calculatorScreen.textContent = calculatorObj.num1;
         }
 
         if (calculatorObj.num1) {
             if (operator.textContent === '+') {
-                calculatorObj.perform = 'Addition'
+                operator.classList.add('selected');
+                calculatorObj.perform = 'Addition';
+                decimalPointAdded = false;
             } else if (operator.textContent === '-') {
-                calculatorObj.perform = 'Subtraction'
+                operator.classList.add('selected');
+                calculatorObj.perform = 'Subtraction';
+                decimalPointAdded = false;
             } else if (operator.textContent === '×') {
-                calculatorObj.perform = 'Multiplication'
+                operator.classList.add('selected');
+                calculatorObj.perform = 'Multiplication';
+                decimalPointAdded = false;
             } else if (operator.textContent === '/') {
-                calculatorObj.perform = 'Division'
+                operator.classList.add('selected');
+                calculatorObj.perform = 'Division';
+                decimalPointAdded = false;
             }
         }
     })
 })
 
 calculatorObj.resetBtn.addEventListener('click', () => {
-    let calculatorScreen = document.querySelector('.calculator__screen')
-    calculatorObj.num1 = ''
-    calculatorObj.num2 = ''
-    calculatorObj.perform = ''
-    calculatorScreen.textContent = '0'
+
+    calculatorObj.operators.forEach((op) => {
+        op.classList.remove('selected');
+    });
+    let calculatorScreen = document.querySelector('.calculator__screen');
+    calculatorObj.num1 = '';
+    calculatorObj.num2 = '';
+    calculatorObj.perform = '';
+    calculatorScreen.textContent = '0';
 })
 
 calculatorObj.deleteBtn.addEventListener('click', () => {
+
+    calculatorObj.operators.forEach((op) => {
+        op.classList.remove('selected');
+    });
+
     if (!calculatorObj.num2) {
-        let calculatorScreen = document.querySelector('.calculator__screen')
-        calculatorObj.perform = ''
-        calculatorObj.num1 = calculatorObj.num1.slice(0, -1)
-        calculatorScreen.textContent = calculatorObj.num1 || 0
+        let calculatorScreen = document.querySelector('.calculator__screen');
+        calculatorObj.perform = '';
+        calculatorObj.num1 = calculatorObj.num1.slice(0, -1);
+        calculatorScreen.textContent = calculatorObj.num1 || 0;
     } else {
-        let calculatorScreen = document.querySelector('.calculator__screen')
-        calculatorObj.num2 = calculatorObj.num2.slice(0, -1)
-        calculatorScreen.textContent = calculatorObj.num2 || 0
+        let calculatorScreen = document.querySelector('.calculator__screen');
+        calculatorObj.num2 = calculatorObj.num2.slice(0, -1);
+        calculatorScreen.textContent = calculatorObj.num2 || 0;
     }
 })
 
+calculatorObj.equalsBtn.addEventListener('click', () => {
+
+    calculatorObj.operators.forEach((op) => {
+        op.classList.remove('selected');
+    });
+    calculatorObj.num2 = '';
+    calculatorScreen.textContent = calculatorObj.num1;
+});
 
